fdbcc07c6608c39ea65c29bbc3f6fb3251a2cba6
Gabriel Ferreira <113285710+GendPh@users.noreply.github.com>
2024-05-21 11:29:36 +0100

    chore: Update background image positioning and add lazy loading for images

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,8 +1,9 @@
 import { Component, HostBinding } from '@angular/core';
 import { routeAnimationTrigger } from '../shared/triggerAnimations';
-import { CommonModule } from '@angular/common';
+import { CommonModule, } from '@angular/common';
 import { LocationsDestinationComponent } from '../locations-destination/locations-destination.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, } from '@angular/forms';
+
 
 @Component({
   selector: 'app-contact',
@@ -14,10 +15,34 @@ import { FormsModule } from '@angular/forms';
 })
 export class ContactComponent {
   @HostBinding('@routeAnimationTrigger') routeAnimation = true;
+
   nameInput: string = '';
   emailInput: string = '';
+  emailErrorMessage: string = 'Can´t be Empty';
   phoneInput: string = '';
   messageInput: string = '';
+
+  SetName(e: Event): void {
+    const inputElement = e.target as HTMLInputElement;
+    this.nameInput = inputElement.value;
+  }
+
+  SetEmail(e: Event): void {
+    const inputElement = e.target as HTMLInputElement;
+    this.emailInput = inputElement.value;
+  }
+  SetPhone(e: Event): void {
+    const inputElement = e.target as HTMLInputElement;
+    this.phoneInput = inputElement.value;
+  }
+  SetMessage(e: Event): void {
+    const inputElement = e.target as HTMLInputElement;
+    this.messageInput = inputElement.value;
+  }
+
+
+
+
 
   OnBlur(event: FocusEvent) {
     const inputElement = event.target as HTMLInputElement;
@@ -31,39 +56,51 @@ export class ContactComponent {
   OnSubmit(e: Event) {
     e.preventDefault();
 
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+    const nameInputElement = document.querySelector('#nameInputEl + .error');
+    const emailInputElement = document.querySelector('#emailInputEl + .error');
+    const phoneInputElement = document.querySelector('#phoneInputEl + .error');
+    const messageInputElement = document.querySelector('#messageInputEl + .error');
+
     if (this.nameInput === '') {
-      const nameInputElement = document.querySelector('#nameInputEl + .error');
       if (nameInputElement) {
         nameInputElement.classList.replace('hide', 'show');
       }
     }
 
     if (this.emailInput === '') {
-      const emailInputElement = document.querySelector('#emailInputEl + .error');
       if (emailInputElement) {
+        this.emailErrorMessage = 'Can´t be Empty';
         emailInputElement.classList.replace('hide', 'show');
       }
     }
 
+    if (!emailRegex.test(this.emailInput) && this.emailInput !== '') {
+      if (emailInputElement) {
+        emailInputElement.classList.replace('hide', 'show');
+        this.emailErrorMessage = 'Please use a valid email address';
+      }
+    }
+
     if (this.phoneInput === '') {
-      const phoneInputElement = document.querySelector('#phoneInputEl + .error');
       if (phoneInputElement) {
         phoneInputElement.classList.replace('hide', 'show');
       }
     }
 
     if (this.messageInput === '') {
-      const messageInputElement = document.querySelector('#messageInputEl + .error');
       if (messageInputElement) {
         messageInputElement.classList.replace('hide', 'show');
       }
     }
 
-    
-
-    console.log('Name: ' + this.nameInput);
-    console.log('Email: ' + this.emailInput);
-    console.log('Phone: ' + this.phoneInput);
-    console.log('Message: ' + this.messageInput);
+    if (this.nameInput !== '' && this.emailInput !== '' && emailRegex.test(this.emailInput) && this.phoneInput !== '' && this.messageInput !== '') {
+      this.nameInput = '';
+      this.emailInput = '';
+      this.phoneInput = '';
+      this.messageInput = '';
+    }
   }
 }
+
